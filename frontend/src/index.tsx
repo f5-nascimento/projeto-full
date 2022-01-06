560b2baa69b752445fe792f299a86b62e7f816af
Felippe Nascimento <nascimento.conceito@gmail.com>
2022-01-05 23:49:19 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
+
 
 
 ReactDOM.render(
@@ -9,4 +12,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+ 
