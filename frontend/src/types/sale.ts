3736eab074f7fb9e6aacd68779e1808e93b7ce68
Felippe Nascimento <nascimento.conceito@gmail.com>
2022-01-08 13:39:44 -0300

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -18,7 +18,7 @@ export type SalePage ={
     totalElements: number;
     totalPages: number;
     size?: number;
-    number?: number;
+    number: number;
     numberOfElements?: number;
     first: boolean;
     empty?: boolean;
